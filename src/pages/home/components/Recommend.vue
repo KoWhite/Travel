8e2555c1de0b082fc4038a98ed700513b62967c9
MichLiu <602502634@qq.com>
2018-11-20 20:39:07 +0800

    banner finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,69 +1,84 @@
 <template>
-    <article>
-        <section class="title">
-            热销推荐
-        </section>
-        <ul>
-            <li class="item border-bottom" v-for="item of list" :key="item.id">
-                <img class="item-img" :src="item.imgUrl" />
-                <div class="item-info">
-                    <p class="item-title">{{item.title}}</p>
-                    <p class="item-desc">{{item.desc}}</p>
-                    <button class="item-button">查看详情</button>
-                </div>
-            </li>
-        </ul>
-    </article>
+  <article>
+    <section class="title">
+      热销推荐
+    </section>
+    <ul>
+      <router-link 
+        tag="li" 
+        class="item border-bottom" 
+        v-for="item of list" 
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </router-link>
+    </ul>
+  </article>
 </template>
 
 <script>
 export default {
-    name: 'HomeRecommend',
+    name: "HomeRecommend",
     props: {
-        list: Array
+      list: Array
     }
-}
+  };
+
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/mixins.styl'
+  @import '~styles/mixins.styl';
 
-    .title
-        margin-top: .2rem
-        line-height: .8rem
-        background: #eee
-        text-indent: .2rem
-    
-    .item
-        overflow: hidden
-        display: flex
-        height: 1.9rem
+  .title {
+    margin-top: 0.2rem;
+    line-height: 0.8rem;
+    background: #eee;
+    text-indent: 0.2rem;
+  }
 
-        .item-img
-            width: 1.7rem
-            height: 1.7rem
-            padding: .1rem
+  .item {
+    overflow: hidden;
+    display: flex;
+    height: 1.9rem;
 
-        .item-info
-            flex: 1
-            padding: .1rem
-            min-width: 0
+    .item-img {
+      width: 1.7rem;
+      height: 1.7rem;
+      padding: 0.1rem;
+    }
 
-            .item-title
-                line-height: .4rem
-                font-size: .3rem
-                ellipsis()
-            .item-desc
-                line-height: .4rem
-                color: #ccc
-                ellipsis()
-            
-            .item-button
-                line-height: .4rem
-                margin-top: .16rem
-                background: #ff9300
-                padding: 0 .1rem
-                border-radius: .06rem
-                color: white
+    .item-info {
+      flex: 1;
+      padding: 0.1rem;
+      min-width: 0;
+
+      .item-title {
+        line-height: 0.4rem;
+        font-size: 0.3rem;
+        ellipsis();
+      }
+
+      .item-desc {
+        line-height: 0.4rem;
+        color: #ccc;
+        ellipsis();
+      }
+
+      .item-button {
+        line-height: 0.4rem;
+        margin-top: 0.16rem;
+        background: #ff9300;
+        padding: 0 0.1rem;
+        border-radius: 0.06rem;
+        color: white;
+      }
+    }
+  }
+
 </style>
-
